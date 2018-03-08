7016d91301bc0acf6a07925c459c420450fcbf03
Sri Sathwik <srisrinivasa.p@hcl.com>
2018-03-08 10:30:47 +0530

    Initial commit

---
+++ src/app/reducers/userReducer.js
@@ -0,0 +1,18 @@
+const userReducer = (state = { name: 'Sri', age: 20 },  action) => {
+    switch (action.type) {
+        case 'SET_NAME':
+            state = {
+                ...state,
+                name: action.payload
+            };
+            break;
+        case 'SET_AGE':
+            state = {
+                ...state,
+                age: action.payload
+            };
+            break;
+    }
+    return state;
+}
+export default userReducer;
