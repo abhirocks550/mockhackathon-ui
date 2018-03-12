2c5ef8144e86e467cc65502c065ca2d41bfac8c8
Sri Sathwik <srisrinivasa.p@hcl.com>
2018-03-12 12:58:11 +0530

    added add_course reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
-import user from './reducers/userReducer';
-const store = createStore(combineReducers({ user }), {}, applyMiddleware(logger, thunk));
+import addCourse from './reducers/addCourseReducer';
+
+const store = createStore(combineReducers({ addCourse }), {}, applyMiddleware(logger, thunk));
 export default store;
