243a1300af54186c29d0e7b50ffdbaed006feb67
Sri Sathwik <srisrinivasa.p@hcl.com>
2018-03-12 12:52:15 +0530

    add css-loader in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,9 @@ var config = {
                 query: {
                     presets: ["react", "es2015", "stage-2"]
                 }
+            }, {
+                test: /\.css$/,
+                loaders: ['style', 'css']
             }
         ]
     }
