86718ca5c7f1beea3d996cbc2c2f974f424452db
Sri Sathwik <srisrinivasa.p@hcl.com>
2018-03-12 12:51:30 +0530

    added react-router v3.2.0

--- src/app/container/App.js
+++ src/app/container/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import { User } from '../components/User';
-import { Main } from '../components/Main';
 import { connect } from 'react-redux';
-import { setName } from '../actions/userActions';
+import AddCourse from './AddCourse';
+import NavBar from '../components/NavigationBar/NavigationBar';
+
 class App extends React.Component {
-
-  changeName =  (name) => {
-      this.props.setName(name);
-    }
-
   render() {
     return (
-        <div className="container">
-                <Main changeUsername={(name) => this.changeName(name)} />
-                <User username={this.props.user.name} />
-            </div>
+      <div className="container">
+        <NavBar />
+        {this.props.children}
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        user: state.user,
-      };
-  };
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        setName: (name) => { dispatch(setName(name)); },
-      };
-  };
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/app/index.js
+++ src/app/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import  App  from './container/App';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import App from './container/App';
+import AddCourse from './container/AddCourse';
 
 render(
     <Provider store={store}>
-        <App />
+        <Router history={browserHistory}>
+            <Route path="/" component={App} >
+                <IndexRoute component={AddCourse} />
+                <Route path="/" component={AddCourse}/>
+            </Route>
+        </Router>
     </Provider>, window.document.getElementById('app'));
